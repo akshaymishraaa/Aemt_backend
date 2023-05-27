d0f832b95d16568756b9086cf238bde5e5c65c62
Akshay mishra <akshaymishra1228@gmail.com>
2023-05-27 17:30:50 +0530

    Changes

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
+const mongoString = process.env.DATABASE_URL
+mongoose.connect(mongoString);
+const database = mongoose.connection
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+// const DataBase_Url = "mongodb+srv://akshaymishra1228:<OpgTPlkL8iZ4ehnj>@aemtdb.dcloy9k.mongodb.net/"
 
 app.use(express.json());
 
 app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
+    console.log(`Server Started at ${3000,mongoString}`)
 })
