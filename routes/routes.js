7355fb3dc21f0e1aadc223148e39e2ed04059d3d
Akshay mishra <akshaymishra1228@gmail.com>
2023-09-27 23:51:38 +0530

    Dashboard UI

--- routes/routes.js
+++ routes/routes.js
@@ -3,18 +3,19 @@ const router = express.Router();
 const Model = require("../model/model");
 
 router.post("/registerUser", async(req, res) => {
-   console.log('7....',req.body)
+   console.log('7....',body)
+   const body = req.body.data;
   const data = new Model({
-    organizationName: req.body.organizationName,
-    organizationEmailId: req.body.organizationEmailId,
-    contactNumber: req.body.contactNumber,
-    organizationType: req.body.organizationType,
-    address: req.body.address,
-    country: req.body.country,
-    state: req.body.state,
-    city: req.body.city,
-    zipCode: req.body.zipCode,
-    regEmpId: req.body.regEmpId,
+    organizationName: body.organizationName,
+    organizationEmailId: body.organizationEmailId,
+    contactNumber: body.contactNumber,
+    organizationType: body.organizationType,
+    address: body.address,
+    country: body.country,
+    state: body.state,
+    city: body.city,
+    zipCode: body.zipCode,
+    regEmpId: body.regEmpId,
   });
   try {
     const dataToSave = await data.save();
